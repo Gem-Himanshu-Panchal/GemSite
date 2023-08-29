25029274e8763bbdde668b52f62b6900af8a54e3
Himanshu.panchal <himanshu.panchal@geminisolutions.com>
2023-08-29 20:33:46 +0530

    Script fixing

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace GemSite
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
